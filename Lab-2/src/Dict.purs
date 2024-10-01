c46c445814823c0513befa0878ba902743d9316c
RAZRULETEL <qwerty.qwertlain@gmail.com>
2024-10-02 02:52:28 +0300

    Implement dict insert

--- Lab-2/src/Dict.purs
+++ Lab-2/src/Dict.purs
@@ -3,6 +3,7 @@ module Dict where
 import Prelude
 
 import Data.Maybe (Maybe, Maybe(Nothing), Maybe(..))
+import Data.Boolean (otherwise)
 
 newtype DictNode a b = CreateDictNode
   { key :: a
@@ -23,3 +24,11 @@ length (CreateDict dict) = lengthInternal dict.root
     Nothing -> 0
     Just (CreateDictNode node) -> 1 + (lengthInternal node.leftLeaf) + (lengthInternal node.rightLeaf)
 
+insert :: forall a b. Ord a => Dict a b -> a -> b -> Dict a b
+insert (CreateDict dict) key value = CreateDict ({root: Just $ insertInternal dict.root})
+  where
+  insertInternal :: Ord a => Maybe (DictNode a b) -> DictNode a b
+  insertInternal Nothing = CreateDictNode({ key: key, value: value, leftLeaf: Nothing, rightLeaf: Nothing })
+  insertInternal (Just (CreateDictNode node)) | key > node.key = CreateDictNode({ key: node.key, value: node.value, leftLeaf: node.leftLeaf, rightLeaf: Just $ insertInternal node.rightLeaf })
+                                                        | key < node.key = CreateDictNode({ key: node.key, value: node.value, leftLeaf: Just $ insertInternal node.leftLeaf, rightLeaf: node.rightLeaf })
+                                                        | otherwise = (CreateDictNode node) -- duplicates not allowed
--- Lab-2/src/Main.purs
+++ Lab-2/src/Main.purs
@@ -4,27 +4,31 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
-import Dict (Dict(..), DictNode(..), length)
+import Dict (Dict(..), DictNode(..), insert, length)
 import Data.Show (show)
 import Data.Maybe (Maybe(Nothing), Maybe(Just))
 
-empty :: Dict Int
+empty :: Dict Int Int
 empty = CreateDict ({ root: Nothing })
 
-nonEmpty :: Dict Int
+nonEmpty :: Dict Int Int
 nonEmpty = CreateDict ({ root: Just zero })
   where
   zero = CreateDictNode ({ key: 0, value: 0, leftLeaf: Nothing, rightLeaf: Nothing })
 
-multiLayer :: Dict Int
+multiLayer :: Dict Int Int
 multiLayer = CreateDict ({ root: Just first })
   where
   third = CreateDictNode ({ key: 0, value: 0, leftLeaf: Nothing, rightLeaf: Nothing })
   second = CreateDictNode ({ key: 0, value: 0, leftLeaf: Nothing, rightLeaf: Just third })
   first = CreateDictNode ({ key: 0, value: 0, leftLeaf: Just second, rightLeaf: Nothing })
 
+insertEls :: Dict Int Int
+insertEls = insert (insert (insert empty 1 1) 2 2) 1 1
+
 main :: Effect Unit
 main = do
   log $ show $ length empty
   log $ show $ length nonEmpty
   log $ show $ length multiLayer
+  log $ show $ length insertEls
