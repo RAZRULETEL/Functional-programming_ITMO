91c7c85e911cdc133e9ad8e05531267e75e5eb6b
RAZRULETEL <qwerty.qwertlain@gmail.com>
2024-10-02 03:10:20 +0300

    Refactor, replace CreateDictNode with singletonNode where possible

--- Lab-2/src/Dict.purs
+++ Lab-2/src/Dict.purs
@@ -24,26 +24,28 @@ length (CreateDict dict) = lengthInternal dict.root
     Nothing -> 0
     Just (CreateDictNode node) -> 1 + (lengthInternal node.leftLeaf) + (lengthInternal node.rightLeaf)
 
-insert :: forall a b. Ord a => Dict a b -> a -> b -> Dict a b
-insert (CreateDict dict) key value = CreateDict ({root: Just $ insertInternal dict.root})
-  where
-  insertInternal :: Ord a => Maybe (DictNode a b) -> DictNode a b
-  insertInternal Nothing = CreateDictNode({ key: key, value: value, leftLeaf: Nothing, rightLeaf: Nothing })
-  insertInternal (Just (CreateDictNode node)) | key > node.key = CreateDictNode({ key: node.key, value: node.value, leftLeaf: node.leftLeaf, rightLeaf: Just $ insertInternal node.rightLeaf })
-                                                        | key < node.key = CreateDictNode({ key: node.key, value: node.value, leftLeaf: Just $ insertInternal node.leftLeaf, rightLeaf: node.rightLeaf })
-                                                        | otherwise = (CreateDictNode node) -- duplicates not allowed
-
 singleton :: forall a b. a -> b -> Dict a b
-singleton key value = CreateDict ({root: Just $ singletonNode key value Nothing Nothing})
+singleton key value = CreateDict ({ root: Just $ singletonNode key value Nothing Nothing })
 
 singletonNode :: forall a b. a -> b -> Maybe (DictNode a b) -> Maybe (DictNode a b) -> DictNode a b
-singletonNode key value leftLeaf rightLeaf = CreateDictNode({ key: key, value: value, leftLeaf: leftLeaf, rightLeaf: rightLeaf })
+singletonNode key value leftLeaf rightLeaf = CreateDictNode ({ key: key, value: value, leftLeaf: leftLeaf, rightLeaf: rightLeaf })
+
+insert :: forall a b. Ord a => Dict a b -> a -> b -> Dict a b
+insert (CreateDict dict) key value = CreateDict ({ root: Just $ insertInternal dict.root })
+  where
+  insertInternal :: Ord a => Maybe (DictNode a b) -> DictNode a b
+  insertInternal Nothing = singletonNode key value Nothing Nothing
+  insertInternal (Just (CreateDictNode node))
+    | key > node.key = singletonNode node.key node.value node.leftLeaf $ Just $ insertInternal node.rightLeaf
+    | key < node.key = singletonNode node.key node.value (Just $ insertInternal node.leftLeaf) node.rightLeaf
+    | otherwise = (CreateDictNode node) -- duplicates not allowed
 
 remove :: forall a b. Ord a => Dict a b -> a -> Dict a b
-remove (CreateDict dict) key = CreateDict ({root: removeInternal dict.root})
+remove (CreateDict dict) key = CreateDict ({ root: removeInternal dict.root })
   where
   removeInternal :: Ord a => Maybe (DictNode a b) -> Maybe (DictNode a b)
   removeInternal Nothing = Nothing -- nothing to do with empty dict
-  removeInternal (Just (CreateDictNode node)) | key > node.key = Just $ singletonNode node.key node.value node.leftLeaf $ removeInternal node.rightLeaf
-                                              | key < node.key = Just $ singletonNode node.key node.value (removeInternal node.leftLeaf) node.rightLeaf
-                                              | otherwise = Nothing -- remove on find
+  removeInternal (Just (CreateDictNode node))
+    | key > node.key = Just $ singletonNode node.key node.value node.leftLeaf $ removeInternal node.rightLeaf
+    | key < node.key = Just $ singletonNode node.key node.value (removeInternal node.leftLeaf) node.rightLeaf
+    | otherwise = Nothing -- remove on find
