838ab412b6155f3deaa4f49a8556cb53ddff0f49
RAZRULETEL <qwerty.qwertlain@gmail.com>
2024-10-05 02:25:03 +0300

    Update tests to check balance of tree

--- Lab-2/test/Dict.purs
+++ Lab-2/test/Dict.purs
@@ -6,8 +6,13 @@ import Effect (Effect)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
-import Dict (balanceTree, get, height, insert, length, remove, singleton)
-import Data.Maybe (Maybe(Just))
+import Dict (get, height, insert, length, remove, singleton)
+import Data.Maybe (Maybe(Just), Maybe(Nothing))
+
+emptyDict = remove (singleton 1 5) 1
+soloDict = singleton 1 5
+simpleRotatedDict = insert (insert (insert (singleton 1 2) 3 4) 5 6) 7 8
+difficultRotatedDict = insert (insert (insert (insert (singleton 7 8) 5 6) 9 10) 1 2) 3 4
 
 testDict :: Effect Unit
 testDict =
@@ -17,30 +22,39 @@ testDict =
     suite "length" do
       test "0"
         $ Assert.equal 0
-        $ length $ remove (singleton 1 5) 1
+        $ length emptyDict
       test "1"
         $ Assert.equal 1
-        $ length (singleton 1 5)
+        $ length soloDict
       test "4"
         $ Assert.equal 4
-        $ length $ (insert (insert (insert (singleton 1 2) 3 4) 5 6) 7 8)
+        $ length simpleRotatedDict
+      test "5"
+        $ Assert.equal 5
+        $ length difficultRotatedDict
     suite "height" do
       test "0"
         $ Assert.equal 0
-        $ height $ remove (singleton 1 5) 1
+        $ height emptyDict
       test "1"
         $ Assert.equal 1
-        $ height (singleton 1 5)
-      test "4 (not balanced)" -- TODO: remove when auto balancing implemented
-        $ Assert.equal 4
-        $ height $ (insert (insert (insert (singleton 1 2) 3 4) 5 6) 7 8)
-      test "4 (balanced to 3)"
+        $ height soloDict
+      test "3"
         $ Assert.equal 3
-        $ height $ balanceTree (insert (insert (insert (singleton 1 2) 3 4) 5 6) 7 8)
+        $ height simpleRotatedDict
+      test "3"
+        $ Assert.equal 3
+        $ height difficultRotatedDict
     suite "get" do
-      test "from root"
+      test "from empty"
+        $ Assert.equal Nothing
+        $ get emptyDict 1
+      test "from solo"
         $ Assert.equal (Just 5)
-        $ get (singleton 1 5) 1
-      test "from leaf"
-        $ Assert.equal (Just 4)
-        $ get (insert (singleton 1 2) 3 4) 3
+        $ get soloDict 1
+      test "from simple rotated"
+        $ Assert.equal (Just 2)
+        $ get simpleRotatedDict 1
+      test "from difficult rotated"
+        $ Assert.equal (Just 2)
+        $ get simpleRotatedDict 1
