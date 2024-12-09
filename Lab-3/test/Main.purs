aaf704c7ea9f2eef12c957a0fffece8044ad67ef
RAZRULETEL <qwerty.qwertlain@gmail.com>
2024-12-10 01:02:07 +0300

    Fix tests

--- Lab-3/test/Main.purs
+++ Lab-3/test/Main.purs
@@ -8,7 +8,7 @@ import Test.Unit (suite, test)
 import Test.Unit.Assert (equal) as Assert
 import Interpolation (linearInterpolate, newtonInterpolate)
 import Data.List (fromFoldable, foldl) as List
-import Data.Tuple (Tuple(Tuple))
+import Data.Tuple (Tuple(Tuple), snd)
 import Data.Monoid (mempty) as List
 import Data.Number (sin)
 import Data.List (foldl, range)
@@ -18,64 +18,66 @@ import Effect.Console (log)
 
 main :: Effect Unit
 main = do
-  log "Test"
---  runTest do
---    test "Conveyor interpolation" do
---      Assert.equal true true
---    suite "Linear interpolation" do
---      test "[]"
---        $ Assert.equal List.mempty
---        $ linearInterpolate (List.fromFoldable []) 1.0
---      test "[1 2, 3 4]"
---        $ Assert.equal (List.fromFoldable [ Tuple 1.0 2.0, Tuple 2.0 3.0, Tuple 3.0 4.0 ])
---        $ linearInterpolate (List.fromFoldable [ Tuple 1.0 2.0, Tuple 3.0 4.0 ]) 1.0
---      test "[1 10000, 3 2, 9 100]"
---        $ Assert.equal (List.fromFoldable [ Tuple 2.0 (-10.0), Tuple 3.0 (-7.5), Tuple 4.0 (-5.0), Tuple 5.0 (-2.5), Tuple 6.0 0.0, Tuple 7.0 2.5, Tuple 8.0 5.0, Tuple 9.0 7.5, Tuple 10.0 10.0 ])
---        $ linearInterpolate (List.fromFoldable [ Tuple 1.0 10000.0, Tuple 2.0 (-10.0), Tuple 10.0 10.0 ]) 1.0
---    suite "Newton interpolation" do
---      test "[]"
---        $ Assert.equal List.mempty
---        $ newtonInterpolate (List.fromFoldable []) 1.0
---      test "[1 2, 3 4]"
---        $ Assert.equal List.mempty
---        $ newtonInterpolate (List.fromFoldable [ Tuple 1.0 2.0, Tuple 3.0 4.0 ]) 1.0
---      test "[1 2, 3 4, 5 6, 7 8]"
---        $ Assert.equal
---            ( List.fromFoldable
---                [ Tuple 1.0 2.0
---                , Tuple 2.0 3.0
---                , Tuple 3.0 4.0
---                , Tuple 4.0 5.0
---                , Tuple 5.0 6.0
---                , Tuple 6.0 7.0
---                , Tuple 7.0 8.0
---                , Tuple 8.0 9.000000000000002
---                , Tuple 9.0 10.000000000000004
---                ]
---            )
---        $ newtonInterpolate (List.fromFoldable [ Tuple 1.0 2.0, Tuple 3.0 4.0, Tuple 5.0 6.0, Tuple 7.0 8.0, Tuple 9.0 10.0 ]) 1.0
---      test "[0 0, 0.5 0.47943, 1.5 0.99749, 3 0.14112, 3.14 0] (sin)"
---        $ Assert.equal true
---        $ foldl
---            (\acc (Tuple _ y) -> acc && (y - sin (y)) < 0.0001)
---            true
---            ( map
---                (\(Tuple x y) -> Tuple x $ y - sin (x))
---                $ newtonInterpolate
---                    ( List.fromFoldable
---                        [ Tuple 0.0 0.0
---                        , Tuple 0.5 $ sin (0.5)
---                        , Tuple 1.5 $ sin (1.5)
---                        , Tuple 3.0 $ sin (3.0)
---                        , Tuple 3.14 0.0
---                        ]
---                    )
---                    0.1
---            )
---    suite "Generate" do
---      test "step 1, start 1, end 1.5"
---        $ Assert.equal (List.fromFoldable [ 1.0, 2.0 ])
---        $ generate 1.0 1.0 1.5
---      test "step 1, start -1, end 15"
---        $ Assert.equal (map toNumber $ range (-1) 15)
---        $ generate 1.0 (-1.0) 15.0
+  runTest do
+    test "Conveyor interpolation" do
+      Assert.equal true true
+    suite "Linear interpolation" do
+      test "[]"
+        $ Assert.equal (Tuple 0 List.mempty)
+        $ linearInterpolate (List.fromFoldable []) 1.0
+      test "[1 2, 3 4]"
+        $ Assert.equal (Tuple 2 (List.fromFoldable [ Tuple 1.0 2.0, Tuple 2.0 3.0, Tuple 3.0 4.0 ]))
+        $ linearInterpolate (List.fromFoldable [ Tuple 1.0 2.0, Tuple 3.0 4.0 ]) 1.0
+      test "[1 10000, 3 2, 9 100]"
+        $ Assert.equal (Tuple 2 (List.fromFoldable [ Tuple 2.0 (-10.0), Tuple 3.0 (-7.5), Tuple 4.0 (-5.0), Tuple 5.0 (-2.5), Tuple 6.0 0.0, Tuple 7.0 2.5, Tuple 8.0 5.0, Tuple 9.0 7.5, Tuple 10.0 10.0 ]))
+        $ linearInterpolate (List.fromFoldable [ Tuple 1.0 10000.0, Tuple 2.0 (-10.0), Tuple 10.0 10.0 ]) 1.0
+    suite "Newton interpolation" do
+      test "[]"
+        $ Assert.equal (Tuple 0 List.mempty)
+        $ newtonInterpolate (List.fromFoldable []) 1.0
+      test "[1 2, 3 4]"
+        $ Assert.equal (Tuple 2 List.mempty)
+        $ newtonInterpolate (List.fromFoldable [ Tuple 1.0 2.0, Tuple 3.0 4.0 ]) 1.0
+      test "[1 2, 3 4, 5 6, 7 8]"
+        $ Assert.equal
+            ( Tuple 5
+                ( List.fromFoldable
+                    [ Tuple 1.0 2.0
+                    , Tuple 2.0 3.0
+                    , Tuple 3.0 4.0
+                    , Tuple 4.0 5.0
+                    , Tuple 5.0 6.0
+                    , Tuple 6.0 7.0
+                    , Tuple 7.0 8.0
+                    , Tuple 8.0 9.000000000000002
+                    , Tuple 9.0 10.000000000000004
+                    ]
+                )
+            )
+        $ newtonInterpolate (List.fromFoldable [ Tuple 1.0 2.0, Tuple 3.0 4.0, Tuple 5.0 6.0, Tuple 7.0 8.0, Tuple 9.0 10.0 ]) 1.0
+      test "[0 0, 0.5 0.47943, 1.5 0.99749, 3 0.14112, 3.14 0] (sin)"
+        $ Assert.equal true
+        $ foldl
+            (\acc (Tuple _ y) -> acc && (y - sin (y)) < 0.0001)
+            true
+            ( map
+                (\(Tuple x y) -> Tuple x $ y - sin (x))
+                $ snd
+                $ newtonInterpolate
+                    ( List.fromFoldable
+                        [ Tuple 0.0 0.0
+                        , Tuple 0.5 $ sin (0.5)
+                        , Tuple 1.5 $ sin (1.5)
+                        , Tuple 3.0 $ sin (3.0)
+                        , Tuple 3.14 0.0
+                        ]
+                    )
+                    0.1
+            )
+    suite "Generate" do
+      test "step 1, start 1, end 1.5"
+        $ Assert.equal (List.fromFoldable [ 1.0, 2.0 ])
+        $ generate 1.0 1.0 1.5
+      test "step 1, start -1, end 15"
+        $ Assert.equal (map toNumber $ range (-1) 15)
+        $ generate 1.0 (-1.0) 15.0
